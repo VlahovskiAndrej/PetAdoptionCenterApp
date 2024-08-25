9eb2608bda90c0d1f3d0d110a5b393f4cc11b566
Георгиев Сандра <sandra.georgiev@students.finki.ukim.mk>
2024-08-25 13:43:41 +0200

    ApiController added

---
+++ Aud8/Controllers/API/PacAdminController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PetAdoptionCenter.Domain.Models;
+using PetAdoptionCenter.Service.Interface;
+
+namespace PetAdoptionCenter.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PacAdminController : ControllerBase
+    {
+        private readonly IPetService _petService;
+        private readonly IAdoptionApplicationService _adoptionApplicationService;
+
+        public PacAdminController(IPetService petService, IAdoptionApplicationService adoptionApplicationService)
+        {
+            _petService = petService;
+            _adoptionApplicationService = adoptionApplicationService;
+        }
+
+        [HttpGet("[action]/{id}")]
+        public Pet GetPetDetails(Guid id)
+        {
+            return _petService.GetPetById(id);
+        }
+
+        [HttpGet("[action]/{id}")]
+        public List<Pet> GetMyPets(string id)
+        {
+            return _petService.GetPetsByShelterId(id);
+        }
+
+        [HttpGet("[action]")]
+        public List<Pet> ListAllPets()
+        {
+            return _petService.GetPets();
+        }
+    }
+}
