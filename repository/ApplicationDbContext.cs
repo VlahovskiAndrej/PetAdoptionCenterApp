a9b384dce0af329bfc299032c0b78d583b609ede
SamoilJakjimovski <153727426+SamoilJakjimovski@users.noreply.github.com>
2024-09-16 17:17:48 +0200

    Change db

--- repository/ApplicationDbContext.cs
+++ repository/ApplicationDbContext.cs
@@ -27,7 +27,7 @@ namespace repository
             if (!optionsBuilder.IsConfigured)
             {
                 optionsBuilder.UseSqlServer(
-                   "Server=SAKICASA\\SQLEXPRESS;Database=PAC;Trusted_Connection=True;TrustServerCertificate=True;",
+                   "Server=tcp:pacsqlserver.database.windows.net,1433;Initial Catalog=PetAdoptionCenterAppDb;Persist Security Info=False;User ID=pacadmin;Password=Admin123-;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",
                     sqlServerOptionsAction: sqlOptions =>
                     {
                         sqlOptions.MigrationsAssembly("PetAdoptionCenter.Web");
